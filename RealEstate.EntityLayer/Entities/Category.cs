5c1e6ef92b00e9f06aa88bd9a6d16e15c0f61720
Enes OZCAN <enesozcan@hotmail.com>
2022-09-28 20:52:12 +0300

    Add project files.

---
+++ RealEstate.EntityLayer/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.EntityLayer.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
